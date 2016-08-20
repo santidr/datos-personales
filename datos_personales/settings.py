b9924ef9929004bf5cfb294d26bf5559693a5231
Santi Díaz <kerbindr@gmail.com>
2016-08-20 19:08:49 -0400

    configuracion de STATIC_ROOT

--- datos_personales/settings.py
+++ datos_personales/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = (os.path.join(BASE_DIR, 'static'),)
 
 LOGIN_REDIRECT_URL = reverse_lazy('lista_persona')
 LOGOUT_REDIRECT_URL = reverse_lazy('login')
