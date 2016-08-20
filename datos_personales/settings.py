783903afb09425759748bb93978b1975c1abe465
Santi Díaz <kerbindr@gmail.com>
2016-08-20 19:14:26 -0400

    Modo debug desactivado

--- datos_personales/settings.py
+++ datos_personales/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'j+xn0a^qjzx$43win65%=qpubm=)j0zgww!ub-vf*2p!)-y6)3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
